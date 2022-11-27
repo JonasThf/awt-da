376caae29a8c5b9b111c754ce9788ee8ebd920e4
sth99 <83654706+sth99@users.noreply.github.com>
2022-11-27 14:12:08 +0100

    update App.js
    tried some functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createJson}>
+      ExportJson
+    </button>
+    <button onClick={sayHello}>
+      Say Hello
+    </button>
     </div>
+    
   );
+  function createJson(){
+    const value = {
+      name: document.getElementById('name').value,
+      typeOfAd: document.getElementById('da-type').value,
+      imageResize: document.getElementById('image_resize').value,
+      interactions: document.getElementById('interactions').value,
+    }
+    alert(value.name)
+  
+  }
+  function sayHello(){
+    alert("Hello")
+  }
 }
 
 export default App;
