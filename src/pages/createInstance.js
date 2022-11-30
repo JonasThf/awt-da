bbb007fea093738bf27f4fd0d8ed08618c35831e
Samuel Thiel <samuelthiel@eduroam-141-23-146-87.wlan.tu-berlin.de>
2022-11-30 16:00:22 +0100

    input fields

--- src/pages/createInstance.js
+++ src/pages/createInstance.js
@@ -2,12 +2,10 @@ import React from "react";
 import './CreateInstance.css'
 
 
-
-
 function CreateInstance() {
-
     return (
         <div className="CreateInstance">
+            <h1>Create Instances</h1>
         </div>
         
     )
--- src/pages/createTemplate.js
+++ src/pages/createTemplate.js
@@ -3,35 +3,35 @@ import './CreateTemplate.css';
   
   const CreateTemplate = () => {
     return (
-     
-    <div className = "createTemplate">
-        <input type = "text" id = "name"></input>
-        <label>Display ad type: </label>
-        <select name ="da-type" id = "da-type" defaultValue={'DEFAULT'}>
-            <option value = "half_screen">Half Screen Banner</option>
-            <option value = "l-banner">L-Banner</option>
-            <option value = "below_screen">Below Screen</option>
-        </select>
-        <input type = "checkbox" id = "image_resize" name = "image_resize" value ="image_reseize">
-            <label>Image Resize</label>
-            <label>Interactions:</label>
-            <input type = "checkbox" id = "interaction1" name = "interaction1" value = "redirection">
-                <label> Links to open</label>
-                <input type = "checkbox" id = "interaction2" value = "differentTabs">
-                    <label> Different Tabs</label>
-                    <input type = "checkbox" id = "interaction3" value = "fill_in_forms">
-                        <label> Fill in Forms</label>
-                        <input type = "checkbox" id = "interaction4" value = "minigames">
-                            <label>Minigames</label>
-                        </input>
-                    </input>
-                </input>
-            </input>
-        </input>
-    </div>);
-    
-  }
-export default CreateTemplate;
+        <div className = "createTemplate">
+              <h1>Create Template</h1>
+              <label>Template Name: </label>
+              <input type = "text"></input><br/>
+              <label> Image Resize</label>
+              <input type = "checkbox"></input><br/>
+              <label>Interactions: </label><br/>
+              <label></label>
+              <input type ="checkbox"></input>
+              <label> Links to open</label><br/>
+              <input type ="checkbox"></input>
+              <label>Different Tabs</label><br/>
+              <input type ="checkbox"></input>
+              <label>Fill in Forms</label><br/>
+              <input type ="checkbox"></input>
+              <label> Mini Games</label>
+               </div>
+        
+    );}
+  export default CreateTemplate;
         
 
 
+    // <div className = "createTemplate">
+    //     <input type = "text" id = "name"></input>
+    //     <label>Display ad type: </label>
+    //     <select name ="da-type" id = "da-type" defaultValue={'DEFAULT'}>
+    //         <option value = "half_screen">Half Screen Banner</option>
+    //         <option value = "l-banner">L-Banner</option>
+    //         <option value = "below_screen">Below Screen</option>
+    //     </select>
+    // </div>)
