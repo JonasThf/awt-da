e38fd281818a8651a06d057da10a7134fdbeaef4
sth99 <samuel.thiel@web.de>
2022-12-01 13:11:01 +0100

    json object created in templat

--- src/pages/createTemplate.js
+++ src/pages/createTemplate.js
@@ -3,19 +3,29 @@ import "./CreateTemplate.css";
 
 
 const CreateTemplate = () => {
+
   function submit(){
-    var title = document.getElementById("template name").value
-    var resizing = document.getElementById("image").value
-    var interactions = [document.getElementById("interaction1").value] 
-    interactions[1] = document.getElementById("interaction2").value
-    interactions[2] = document.getElementById("interaction3").value
-    interactions[3] = document.getElementById("interaction4").value
-    var da_type = document.getElementById("da-type").value
-    console.log(da_type)
-    for (var i = 0; i<interactions.length; i++){
-      console.log(interactions[i])
-    }
     
+    var title = document.getElementById("template name").value
+    var resizing = document.getElementById("image").checked
+    var interactions = [document.getElementById("interaction1").checked] 
+    interactions[1] = document.getElementById("interaction2").checked
+    interactions[2] = document.getElementById("interaction3").checked
+    interactions[3] = document.getElementById("interaction4").checked
+    var da_type = document.getElementById("da-type").value
+
+    var  template = {
+      name: title,
+      resize: resizing,
+      ineraction: interactions,
+      daType: da_type,
+      duration: 0, 
+      media_ressource: ""
+  }
+  var templateAsString = JSON.stringify(template)
+  console.log(templateAsString)
+
+      
   }
   return (
     <div className="createTemplate">
@@ -33,16 +43,16 @@ const CreateTemplate = () => {
           <label>Interactions: </label>
           <br/>
           <label></label>
-          <input type="checkbox" id = "interaction1" value = "Links to open"></input>
+          <input type="checkbox" id = "interaction1"></input>
           <label>Links to open</label>
           <br/>
-          <input type="checkbox" id = "interaction2" value = "Different Tabs"></input>
+          <input type="checkbox" id = "interaction2"></input>
           <label>Different Tabs</label>
           <br />
-          <input type="checkbox" id = "interaction3" value = "Fill in Forms"></input>
+          <input type="checkbox" id = "interaction3"></input>
           <label>Fill in Forms</label>
           <br/>
-          <input type="checkbox" id = "interaction4" value = "Mini Games"></input>
+          <input type="checkbox" id = "interaction4"></input>
           <label>Mini Games</label>
           <br/>
           <label>Choose Da-Type:</label>
