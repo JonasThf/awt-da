5dcac920c8f5a6f31f7a8c3279c634ba01114417
Jonas Thierfeldt <JonasThf>
2023-01-17 11:01:32 +0100

    editor: Change page layout and add upload function webserver: Change templates and JSON object

--- awt-da-editor/src/App.js
+++ awt-da-editor/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from "react";
 import './App.css';
 import CreateInstance from './pages/CreateInstance'
 import CreateTemplate from './pages/CreateTemplate';
@@ -6,33 +6,39 @@ import Panel from "./components/Panel";
 import { Row, Col} from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+import { Filecontext } from './Filecontext';
 
 
 function App() {
+  const [imgData, setImgData] = React.useState(null);
   
   return(
-    <Container fluid id="container">
-      <Row>
-        <Col id="header" sm={12}>Welcome to the Display Ad Editor</Col>
-      </Row>
-      <Row id="row">
-        <Col xs={1}></Col> 
-        <Col className="px-0" id="createTemplateCol" xs={2}>
-          <CreateTemplate></CreateTemplate>
-        </Col>
-        <Col id="boxCol" className="px-0" xs={6}>
-          <p id="tv">TV</p>
-          <Panel>content</Panel>
-          <Panel>Panel 2</Panel>
-        </Col>
-        <Col id="createInstanceCol" className="px-0" xs={2}>
-          <CreateInstance></CreateInstance>
-        </Col>
-        <Col xs={1}></Col> 
-      </Row>
-    </Container>
+    <div className="root">
+      <Filecontext.Provider value={{ imgData, setImgData }}>
+        <Container id="container">
+          <Row>
+            <Col md={12} id="header" >Welcome to the Display Ad Editor</Col>
+          </Row>
+          <Row id="row">
+            <Col className="px-0" id="createTemplateCol" md={3}>
+              <CreateTemplate></CreateTemplate>
+            </Col>
+            <Col id="boxCol" className="px-0" md={6}>
+              <p id="tv">TV</p>
+              <Panel>content</Panel>
+              <Panel>Panel 2</Panel>
+              <Image fluid src={imgData} id="uploadImage"/>
+            </Col>
+            <Col id="createInstanceCol" className="px-0" md={3}>
+              <CreateInstance></CreateInstance>
+            </Col>
+          </Row>
+        </Container>
+      </Filecontext.Provider>
+    </div>
   );
-  
+ 
 
   
 }
